022a569a7b25e30647f5d7570685724cf647ce7f
Hideki Ikemoto <ikemo333@gmail.com>
2022-11-27 14:36:24 +0900

    任意の順序でのソート機能(Case, When使用)

--- apps/example/views_example.py
+++ apps/example/views_example.py
@@ -1,5 +1,13 @@
-from django.views.generic import TemplateView
+from django.views.generic import ListView
+
+from apps.example.models import Task
 
 
-class ExampleView(TemplateView):
+class ExampleView(ListView):
     template_name = "example.html"
+    model = Task
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["ordered_list"] = Task.objects.order_by_status()
+        return context
